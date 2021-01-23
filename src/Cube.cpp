79d2157e023a1d78f6260ef52d38fd41cf11138a
morsisko <morsisko@gmail.com>
2021-01-23 03:03:59 +0100

    Added move and solve callback, possibility to determine whenever cube is solved

--- src/Cube.cpp
+++ src/Cube.cpp
@@ -1,8 +1,10 @@
 #include "Cube.h"
 #include <Arduino.h>
 
-Cube* mainCube;
+const uint8_t Cube::encodingData[] = {80, 175, 152, 32, 170, 119, 19, 137, 218, 230, 63, 95, 46, 130, 106, 175, 163, 243, 20, 7, 167, 21, 168, 232, 143, 175, 42, 125, 126, 57, 254, 87, 217, 91, 85, 215};
+const uint8_t Cube::solvedState[] = {0x12, 0x34, 0x56, 0x78, 0x33, 0x33, 0x33, 0x33, 0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC, 0x00, 0x00};
 
+/*
 static void notifyCallback(
   BLERemoteCharacteristic* pBLERemoteCharacteristic,
   uint8_t* pData,
@@ -12,10 +14,40 @@ static void notifyCallback(
     Serial.print("Moved");
 
 }
+*/
+
+void Cube::onNotify(BLERemoteCharacteristic* pBLERemoteCharacteristic, uint8_t* pData, size_t length, bool isNotify)
+{
+	Serial.println("Moved from class!");
+	
+	if (length != 20)
+		return;
+	
+	if (pData[18] != 0xA7)
+		return;
+	
+	decodeMovePacket(pData);
+	
+	if (onMove)
+		onMove(this);
+	
+	if (onSolve && isSolved())
+		onSolve(this);
+}
 
 Cube::Cube()
 {
-	mainCube = this;
+	onMove = nullptr;
+	onSolve = nullptr;
+}
+
+void Cube::decodeMovePacket(const uint8_t* packet)
+{
+	uint8_t high = packet[19] >> 4;
+	uint8_t low = packet[19] & 0xF;
+
+	for (int i = 0; i < 19; i++)
+		currentRawState[i] = packet[i] - encodingData[i + low] - encodingData[i + high];
 }
 
 void Cube::connect(BLEAdvertisedDevice device)
@@ -41,11 +73,32 @@ void Cube::connect(BLEAdvertisedDevice device)
 	
     if(pRemoteCharacteristic->canNotify())
     {
-      pRemoteCharacteristic->registerForNotify(notifyCallback);
+      //pRemoteCharacteristic->registerForNotify(notifyCallback);
+	  pRemoteCharacteristic->setCallbacks(this);
 	  Serial.println("Registered callback!");
     }
 	else
 	{
 		Serial.println("Registration failed!");
 	}
+}
+
+void Cube::setOnMove(MovedCallback onMove)
+{
+	this->onMove = onMove;
+}
+
+void Cube::setOnSolve(SolvedCallback onSolve)
+{
+	this->onSolve = onSolve;
+}
+
+bool Cube::isSolved()
+{
+	for (int i = 0; i < sizeof(solvedState) / sizeof(uint8_t); i++)
+	{
+		if (currentRawState[i] != solvedState[i])
+			return false;
+	}
+	return true;
 }
--- src/Cube.h
+++ src/Cube.h
@@ -7,14 +7,28 @@ static BLEUUID realUUID("0000aadb-0000-1000-8000-00805f9b34fb");
 static BLEUUID charUUID("0000aadc-0000-1000-8000-00805f9b34fb");
 static BLEUUID serviceUUID("0000fe95-0000-1000-8000-00805f9b34fb");
 
-class Cube
+class Cube;
+typedef void (*MovedCallback)(Cube* cube);
+typedef void (*SolvedCallback)(Cube* cube);
+
+class Cube : public BLERemoteCharacteristicCallbacks
 {
 private:
+	static const uint8_t encodingData[];
+	static const uint8_t solvedState[];
+	
+	uint8_t currentRawState[20] = {0};
 	BLEClient* pClient;
+	MovedCallback onMove;
+	SolvedCallback onSolve;
+	
+	void onNotify(BLERemoteCharacteristic* pBLERemoteCharacteristic, uint8_t* pData, size_t length, bool isNotify) override;
+	void decodeMovePacket(const uint8_t* packet);
 	
 public:
 	Cube();
 	void connect(BLEAdvertisedDevice device);
-	
-	
+	void setOnMove(MovedCallback onMove);
+	void setOnSolve(SolvedCallback onSolve);
+	bool isSolved();
 };
